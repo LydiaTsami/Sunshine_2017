55952befe7d5901922336efe453846ee96a0adcb
LydiaTsami <lydia.tsami@gmail.com>
2017-10-08 19:19:58 +0300

    Initial Commit

--- app/src/main/java/android/uom/gr/sunshineuom/MainActivity.java
+++ app/src/main/java/android/uom/gr/sunshineuom/MainActivity.java
@@ -17,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
             "Thurs 6/26 - Rainy - 18/11",
             "Fri 6/27 - Foggy - 21/10",
             "Sat 6/28 - Error! - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    };
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - Error! - 23/18",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - Error! - 23/18",
+            "Sun 6/29 - Sunny - 20/7"         };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         R.id.list_item_forecast_textview,
                         dummyList);
 
-        ListView forecastListView = (ListView)this.findViewById(R.id.listview_forecast);
+        ListView forecastListView = (ListView)findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(forecastListAdapter);
 
     }
