a4508322c39531b136cc0e263cde9dc83de01291
LydiaTsami <lydia.tsami@gmail.com>
2017-10-21 17:20:14 +0300

    fetch data and display raw

---
+++ app/src/main/java/android/uom/gr/sunshineuom/ForecastFragment.java
@@ -0,0 +1,191 @@
+package android.uom.gr.sunshineuom;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ForecastFragment extends Fragment {
+
+    ArrayAdapter<String> forecastListAdapter;
+
+    public ForecastFragment() {
+        // Required empty public constructor
+
+    }
+
+    private String[] data = {
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - Error! - 23/18",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - Error! - 23/18",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - Error! - 23/18",
+            "Sun 6/29 - Sunny - 20/7"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragmentmenu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("734077");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        List<String> dummyList =  new ArrayList<>(Arrays.asList(data));
+
+        forecastListAdapter =
+                new ArrayAdapter<>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_TextView,
+                        dummyList);
+
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(forecastListAdapter);
+
+        return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            return fetchWeatherData();
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if(strings != null){
+                forecastListAdapter.clear();
+                for(String forecast : strings){
+                    forecastListAdapter.add(forecast);
+                }
+            }
+        }
+
+        private String[] fetchWeatherData() {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                //MODIFIED FOR CITY OF THESSALONIKI, GREECE
+                URL url = new URL("http://api.openweathermap.org/data/2.5/" +
+                        "forecast/daily" +
+                        "?id=734077&mode=json&units=metric&cnt=7" +
+                        "&APPID=27949ea6b6dffa1dad1deb925c9b024b");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                }
+                forecastJsonStr = buffer.toString();
+
+                Log.i("ForecastFragment",forecastJsonStr);
+
+                List<String> weatherList =
+                        WeatherJsonParser.getWeatherFromJson(forecastJsonStr, 7);
+
+                String[] wAr = new String[7];
+                return weatherList.toArray(wAr);
+
+            } catch (IOException e) {
+                Log.e("ForecastFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/android/uom/gr/sunshineuom/WeatherJsonParser.java
@@ -0,0 +1,47 @@
+package android.uom.gr.sunshineuom;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lydia on 10/21/2017.
+ */
+
+public class WeatherJsonParser {
+
+    public static List<String> getWeatherFromJson(String jsonString, int numDays){
+        List<String> results = new ArrayList<>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray weatherList = jsonObject.getJSONArray("list");
+
+            for(int i =0; i< weatherList.length(); i++){
+                String day = "day "+i;
+                String condition, low, high;
+
+                JSONObject dayForecast = weatherList.getJSONObject(i);
+                JSONArray weather = dayForecast.getJSONArray("weather");
+                condition = weather.getJSONObject(0).getString("main");
+
+                JSONObject tempObject = dayForecast.getJSONObject("temp");
+                low = tempObject.getString("min");
+                high = tempObject.getString("max");
+
+                String res = day + " - "+condition+" - "+ low+" / "+high;
+                results.add(res);
+            }
+
+        } catch (JSONException e) {
+            Log.e("WeatherJsonParser", e.getMessage());
+        }
+
+        return results;
+    }
+}
